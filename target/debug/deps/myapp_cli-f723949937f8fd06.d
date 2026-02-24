64c7932fbe21384da2bc49c5310c86296d8ed5c2
nachog00 <nachogeo@zingolabs.org>
2026-02-24 20:03:21 -0300

    Add .gitignore, remove target from tracking

--- target/debug/deps/myapp_cli-f723949937f8fd06.d
+++
@@ -1,5 +0,0 @@
-/tmp/knope-test/target/debug/deps/myapp_cli-f723949937f8fd06.d: crates/cli/src/main.rs
-
-/tmp/knope-test/target/debug/deps/libmyapp_cli-f723949937f8fd06.rmeta: crates/cli/src/main.rs
-
-crates/cli/src/main.rs:
--- target/debug/deps/myapp_utils-6b21d53f0efc2a63.d
+++
@@ -1,5 +0,0 @@
-/tmp/knope-test/target/debug/deps/myapp_utils-6b21d53f0efc2a63.d: crates/utils/src/lib.rs
-
-/tmp/knope-test/target/debug/deps/libmyapp_utils-6b21d53f0efc2a63.rmeta: crates/utils/src/lib.rs
-
-crates/utils/src/lib.rs:
